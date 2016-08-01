56fa3c63b2df17b3191654ec6b47afeb378f2e98
Robert Williams <robwillcode@gmail.com>
2016-08-01 15:45:13 -0700

    Fixed host_url issue with multiple interfaces.

--- assets/ts/main.ts
+++ assets/ts/main.ts
@@ -19,12 +19,21 @@ app.on('ready', function() {
 
 	var ifaces = os.networkInterfaces();
 	Object.keys(ifaces).forEach(function (ifname) {
+		if(host_url){
+			return true;
+		}
 		ifaces[ifname].forEach(function (iface) {
+			if(host_url){
+				return true;
+			}
+			console.log(iface);
 			if ('IPv4' !== iface.family || iface.internal !== false) {
 				// skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
 				return;
 			}
-			host_url = "http://"+iface.address+":"+PORT+"/";
+			if(!host_url){
+				host_url = "http://"+iface.address+":"+PORT+"/";
+			}
 		});
 	});
 
