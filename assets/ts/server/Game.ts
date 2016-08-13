e96b22c59e1187155a4e56bd6eee1b5a3e1094e9
Josh Fraser <joshua.david.fraser@gmail.com>
2016-08-13 15:51:59 -0700

    Added navigator stuff

--- assets/ts/server/Game.ts
+++ assets/ts/server/Game.ts
@@ -14,7 +14,7 @@ export class Game {
         this.state = {
             'captain': {},
             'engineer': {},
-            'navigation': {},
+            'navigator': {},
             'robotics': {},
             'weapons': {},
         };
