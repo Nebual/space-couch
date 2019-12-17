1ddfa3dffea3b2cd2f864fb9e71d98bfea6b4966
Unknown <robwillcode@gmail.com>
2019-12-16 22:32:36 -0800

    Added component that makes back buttons more intuitive

---
+++ src/components/ConfirmButton.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import classNames from 'classnames'
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Fab from '@material-ui/core/Fab';
+import { makeStyles } from '@material-ui/core/styles';
+import CheckIcon from '@material-ui/icons/Check';
+
+const useStyles = makeStyles(theme => ({
+	root: {
+		display: 'flex',
+		alignItems: 'center',
+	},
+	wrapper: {
+		margin: theme.spacing(1),
+		position: 'relative',
+	},
+	fabProgress: {
+		color: '#35a81d',
+		position: 'absolute',
+		top: -6,
+		left: -6,
+		zIndex: 1,
+		pointerEvents: 'none',
+	},
+}));
+
+export default function CircularIntegration({
+	targetTime = 500,
+	callBack,
+	children,
+	className
+}) {
+	const classes = useStyles();
+	const [loading, setLoading] = React.useState(false);
+	const [progress, setProgress] = React.useState(0);
+	const [start, setStart] = React.useState(null);
+
+	const handleMouseDown = () => {
+		setProgress(0);
+		setLoading(true);
+		setStart(new Date());
+	};
+
+	const handleMouseUp = () => {
+		const end = new Date();
+		if (end - start >= targetTime) {
+			callBack();
+		}
+		setProgress(0);
+		setLoading(false);
+	};
+
+	React.useEffect(() => {
+		function tick() {
+			if (loading) {
+				const timePassed = new Date() - start;
+				const newProgress = Math.round((timePassed / targetTime) * 100);
+
+				setProgress(() => (newProgress > 100 ? 100 : newProgress));
+			}
+		}
+
+		const timer = setInterval(tick, 20);
+		return () => {
+			clearInterval(timer);
+		};
+	}, [loading]);
+
+	return (
+		<div className={classNames(classes.root, className)}>
+			<div className={classes.wrapper}>
+				<Fab
+					aria-label="save"
+					color="primary"
+					onMouseDown={handleMouseDown}
+					onMouseUp={handleMouseUp}
+					onTouchStart={handleMouseDown}
+					onTouchEnd={handleMouseUp}
+				>
+					{progress >= 100 ? <CheckIcon /> : children}
+				</Fab>
+				{loading && (
+					<CircularProgress
+						variant="determinate"
+						size={68}
+						className={classes.fabProgress}
+						value={progress}
+					/>
+				)}
+			</div>
+		</div>
+	);
+}
--- src/roles/List/LobbyButton.js
+++ src/roles/List/LobbyButton.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import classNames from 'classnames';
 import { Redirect, useHistory } from 'react-router-dom';
 import { useClientNet } from '../../client/ClientNet';
+import ConfirmButton from '../../components/ConfirmButton';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 
 let lobby_hold_timer;
 export default function LobbyButton() {
 	const clientNet = useClientNet();
 	const history = useHistory();
+
 	useEffect(() => {
 		clientNet.updateRole();
 	}, [history.location.pathname, clientNet]);
@@ -15,31 +17,24 @@ export default function LobbyButton() {
 		history.location.pathname === '/';
 
 	const [isRedirecting, setIsRedirecting] = useState(false);
-	const lobbyButtonStartTouch = () => {
-		lobby_hold_timer = setTimeout(function() {
-			setIsRedirecting(true);
+
+	useEffect(() => {
+		if (isRedirecting) {
 			setIsRedirecting(false);
-		}, 1000);
-	};
-	const lobbyButtonEndTouch = () => {
-		clearTimeout(lobby_hold_timer);
-	};
+		}
+	}, [isRedirecting]);
+
 	return (
 		<>
 			{isRedirecting && <Redirect to="/" />}
-			<button
-				type="button"
-				className={classNames(
-					'btn btn-link lobby-button',
-					hide && 'hidden'
-				)}
-				onMouseDown={lobbyButtonStartTouch}
-				onMouseUp={lobbyButtonEndTouch}
-				onTouchStart={lobbyButtonStartTouch}
-				onTouchEnd={lobbyButtonEndTouch}
-			>
-				<i className="fa fa-hand-o-left" />
-			</button>
+			{!hide ? (
+				<ConfirmButton
+					className='lobby-button'
+					callBack={() => setIsRedirecting(true)}
+				>
+					<ArrowBackIcon />
+				</ConfirmButton>
+			) : null}
 		</>
 	);
 }
